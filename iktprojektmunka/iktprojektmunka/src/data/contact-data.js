99db20f31fcb5b35664ea12627ed3784dc0fb9b8
GyetvaiAdam <128683867+GyetvaiAdam@users.noreply.github.com>
2024-12-28 18:16:38 +0100

    react

---
+++ iktprojektmunka/iktprojektmunka/src/data/contact-data.js
@@ -0,0 +1,27 @@
+import {
+  HeartIcon,
+  TrashIcon, //majd ide rakok mast csak ne felejtsedm el ong
+} from "@heroicons/react/24/solid";
+
+export const contactData = [
+  {
+    title: "Ongyilkos akarok lenni",
+    icon: HeartIcon,
+    description:
+      "hajnali 3 van es meg mindig itt ulok ember",
+  },
+  {
+    title: "yappatron",
+    icon: TrashIcon,
+    description:
+      "Gooning",
+  },
+  {
+    title: "lemértem 10centi",
+    icon: TrashIcon,
+    description:
+      "még sem inkább 2",
+  },
+];
+
+export default contactData;
---
+++ iktprojektmunka/iktprojektmunka/src/pages/home.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+  Button,
+  IconButton,
+  Input,
+  Textarea,
+  Checkbox,
+} from "@material-tailwind/react";
+import { FingerPrintIcon, UsersIcon } from "@heroicons/react/24/solid";
+import { PageTitle, Footer } from "@/widgets/layout";
+import { FeatureCard, TeamCard } from "@/widgets/cards";
+import { featuresData, teamData, contactData } from "@/data";
+
+export function Home() {
+  return (
+    <>
+      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+        <div
+          className="absolute top-0 h-full w-full bg-cover bg-center"
+          style={{ backgroundImage: "url('/img/e658967d719a8673fb06193b0afb9394c34dcb0f_2_1024x537.webp')" }}
+        />
+        <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
+        <div className="max-w-8xl container relative mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-6 font-black"
+              >
+                Welcome to 16personalities 
+                <p className="text-sm opacity-75 transition-opacity duration-300">Made by Gergencsik Levente, Kárpáti Jázmin</p>
+              </Typography>
+              <Typography variant="lead" color="white" className="opacity-80">
+              This test is designed to help you understand your strengths, weaknesses, and preferences. By taking this test, you will gain valuable insights into your behavior, motivations, and how you interact with others. Start your journey of self-discovery today and unlock your full potential.
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="-mt-32 bg-white px-4 pb-20 pt-4">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map(({ color, title, icon, description }) => (
+              <FeatureCard
+                key={title}
+                color={color}
+                title={title}
+                icon={React.createElement(icon, {
+                  className: "w-5 h-5 text-white",
+                })}
+                description={description}
+              />
+            ))}
+          </div>
+          <div className="mt-32 flex flex-wrap items-center">
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-blue-gray-900 p-2 text-center shadow-lg">
+                <FingerPrintIcon className="h-8 w-8 text-white " />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                blah blah blah bleh bluh blugy
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+                blah blah blah bleh bluh blugy
+                <br />
+                <br />
+                blah blah blah bleh bluh blugy
+              </Typography>
+              <Button variant="filled">read more</Button>
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg border shadow-gray-500/10 rounded-lg">
+                <CardHeader floated={false} className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/uwkwq0meu5na1.jpg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography variant="small" color="blue-gray" className="font-normal">Rocket League</Typography>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 mt-2 font-bold"
+                  >
+                    blah blah blah bleh bluh blugy
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                  blah blah blah bleh bluh blugy
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-4 pt-20 pb-48">
+        <div className="container mx-auto">
+          <PageTitle section="Personalities" heading="Heere are the main personality types">
+          blah blah blah bleh bluh blugy
+          </PageTitle>
+          <div className="mt-24 grid grid-cols-1 gap-12 gap-x-24 md:grid-cols-2 xl:grid-cols-4">
+            {teamData.map(({ img, name, position, socials }) => (
+              <TeamCard
+                key={name}
+                img={img}
+                name={name}
+                position={position}
+                socials={
+                  <div className="flex items-center gap-2">
+                    {socials.map(({ color, name }) => (
+                      <IconButton key={name} color={color} variant="text">
+                        <i className={`fa-brands text-xl fa-${name}`} />
+                      </IconButton>
+                    ))}
+                  </div>
+                }
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="relative bg-white py-24 px-4">
+        <div className="container mx-auto">
+          <PageTitle section="nagyon" heading="Faszom kivan mar">
+          blah blah blah bleh bluh blugy
+          </PageTitle>
+          <div className="mx-auto mt-20 mb-48 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+            {contactData.map(({ title, icon, description }) => (
+              <Card
+                key={title}
+                color="transparent"
+                shadow={false}
+                className="text-center text-blue-gray-900"
+              >
+                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-blue-gray-900 shadow-lg shadow-gray-500/20">
+                  {React.createElement(icon, {
+                    className: "w-5 h-5 text-white",
+                  })}
+                </div>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                  {title}
+                </Typography>
+                <Typography className="font-normal text-blue-gray-500">
+                  {description}
+                </Typography>
+              </Card>
+            ))}
+          </div>
+         </div>
+      </section>
+      <div className="bg-white">
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Home;
